cc9be3e0d717c210121dbe4ea862e7e4800fe6b0
RahulSonawane1 <rahulsonaw2001@gmail.com>
2025-11-18 15:52:43 +0530

    Update CORS and error handling for production

--- db.js
+++ db.js
@@ -6,8 +6,10 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database : process.env.DB_DATABASE,
-    port: process.env.DB_PORT
-
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 (async () => {
@@ -19,6 +21,7 @@ const db = mysql.createPool({
     }
     catch(err) {
         console.error("MySQL connection error: ",err.message);
+        console.error("DB_HOST:", process.env.DB_HOST);
     }
 })();
 
--- index.js
+++ index.js
@@ -11,7 +11,17 @@ const menuRoutes = require('./routes/menuRoutes');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://your-netlify-domain.netlify.app',
+    //'https://yourdomain.com' // if you have a custom domain
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../client')));
